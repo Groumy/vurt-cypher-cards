ba1437183a20700f760fc82d495b332087a1e83c
Dominic Richard <groumy@gmail.com>
2019-03-10 08:02:21 -0400

    layout 0.1.0

--- lib/vurt-cypher-cards.rb
+++ lib/vurt-cypher-cards.rb
@@ -1,21 +1,17 @@
 require 'squib'
+require 'yaml'
 
-Squib::Deck.new(layout: 'data/base-layout.yml') do
+DATA_FOLDER = 'data'.freeze
+LAYOUT_FILE = File.join(DATA_FOLDER, 'base-layout.yml')
+
+Squib::Deck.new(layout: 'data/base-layout.yml', width: 1125, height: 825) do
   background color: 'white'
   # hint text: :black # uncomment to see the text box boundaries
 
-  rect layout: :title_box,
-        fill_color: :deep_sky_blue, stroke_width: 0
-  text str: " A to Z BOOK",  layout: :title
-  # text str: '1d6',             layout: :type_icon
-  text str: '1d6',          layout: :type
-  text str: 'Form',  layout: :title_middle
-  rect fill_color: :black,   layout: :middle_rect
+  text str: "FLASHBANG GRENADE (1d6+3)",  layout: :title
+  text str: "\tFor 10 minutes per level of the cypher, decrease the difficulty of social interaction tasks by one level. Intimidation, persuasion, negotiation—anything involving smoothness. For the next 2 hours, the difficulty of all Speed tasks is increased by one step. The effect can stack up to three times. If three drinks are taken, the character dies at the end of the effect. U-type are immune to this effect. \n\t”Take one measure for a good time, two for a blast, three for a clean and sexy death.”", layout: :rule_block
 
-  text str: 'Provides the PC with an asset when attempting to navigate from one place to another within the city district covered by the A to Z Book. <br />&ensp;Each A to Z Book covers one specific city district. Because the landscape of the city changes so quickly, the map is only useable a number of times equal to the level of the cypher, after which time, it is no longer accurate. A to Z Books can be acquired as a short-term benefit by spending 2 XP and the time to scrounge one up', layout: :rule_top
-  text str: 'A battered old road map or map book that has changed hands a thousand times. The maps are covered with notes and lines, with old streets crossed off and new routes drawn in.', layout: :rule_bottom
-
-  # text str: 'Tiny text', layout: :copyright
+  text str: 'v0.1.0', layout: :copyright
 
   rect layout: :safe
   rect layout: :cut
